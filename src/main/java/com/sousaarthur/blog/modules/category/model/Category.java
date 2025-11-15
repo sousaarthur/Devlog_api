3f3908c057847de14044c2a6a42193d2ac0e1eb4
filoroch <dev.filoroch@gmail.com>
2025-11-15 16:44:36 -0300

    fix(category): adicionado a anotação Entity

--- src/main/java/com/sousaarthur/blog/modules/category/model/Category.java
+++ src/main/java/com/sousaarthur/blog/modules/category/model/Category.java
@@ -1,5 +1,6 @@
 package com.sousaarthur.blog.modules.category.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
