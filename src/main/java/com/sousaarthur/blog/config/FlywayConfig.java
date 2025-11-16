515d7ce0a1aa4f0e3692a09825925e8fddd8566a
filoroch <dev.filoroch@gmail.com>
2025-11-15 22:06:32 -0300

    fix: corrigindo o conflito de migrations

---
+++ src/main/java/com/sousaarthur/blog/config/FlywayConfig.java
@@ -0,0 +1,17 @@
+package com.sousaarthur.blog.config;
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+    @Bean
+    public FlywayMigrationStrategy repairStrategy(){
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+
+    }
+}
