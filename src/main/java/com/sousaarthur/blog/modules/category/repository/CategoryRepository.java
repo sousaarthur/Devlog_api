b71837ff718eddfbe361d2970b93b902a58ae921
filoroch <dev.filoroch@gmail.com>
2025-11-16 00:39:53 -0300

    fix(CategoryRepository): corrigindo o metodo de recuperar nome parcial

--- src/main/java/com/sousaarthur/blog/modules/category/repository/CategoryRepository.java
+++ src/main/java/com/sousaarthur/blog/modules/category/repository/CategoryRepository.java
@@ -2,9 +2,15 @@ package com.sousaarthur.blog.modules.category.repository;
 
 import com.sousaarthur.blog.modules.category.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     Optional<Category> findByName(String categoryName);
+
+    @Query("SELECT c FROM Category c WHERE c.name LIKE %:nome%")
+    List<Category> findByNamePartial(String nome);
+//    List<Category> findByNomeContaining(String nome);
 }
